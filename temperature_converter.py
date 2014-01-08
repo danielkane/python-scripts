14a52a4b59f0493d192204431d19da2a527343e0
tswbyaj <tswbyaj@gmail.com>
2014-01-08 15:06:35 -0800

    created functions for converting temperatures

---
+++ temperature_converter.py
@@ -0,0 +1,12 @@
+
+
+def convert_to_fahrenheit(celcius):
+    Tc = int(celcius)*9 / 5+32
+    return Tc
+
+
+def convert_to_celcius(fahrenheit):
+    Tf = (int(fahrenheit)-32)*5 / 9
+    return Tf
+
+
