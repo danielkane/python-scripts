85632a78ff3b482dec3a7f5b9593316676cf5d84
Daniel Kane <daniel@tractable.us>
2017-08-20 16:07:55 -0700

    wrapping script in a function

--- get_usgs_water_by_state.py
+++ get_usgs_water_by_state.py
@@ -3,12 +3,13 @@ import urllib, json
 # This script extracts the most recent water data by state for all active sites in the USGS database.
 # replace CA in stateCd=CA with the state you want to extract water data from
 
-response = urllib.urlopen('http://waterservices.usgs.gov/nwis/iv/?format=json&stateCd=CA&modifiedSince=PT30M')
-parsed = json.loads(response.read())
-data = json.dumps(parsed, indent=4, sort_keys=True)
+def write_usgs_water_to_file(state="CA"):
+    url = 'http://waterservices.usgs.gov/nwis/iv/?format=json&stateCd={0}&modifiedSince=PT30M'.format(state)
+    response = urllib.urlopen(url)
+    parsed = json.loads(response.read())
+    data = json.dumps(parsed, indent=4, sort_keys=True)
 
-
-data = str(data)
-f = open('usgs_sites_ca.json', 'w')
-f.write(data)
-f.close()
+    data = str(data)
+    f = open('usgs_sites_{0}.json'.format(state.lower()), 'w')
+    f.write(data)
+    f.close()
